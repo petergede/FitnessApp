32e7ab368b7652769ef64c29534971fe13041818
petrosgentekakis <gentekakisp@gmail.com>
2024-04-03 14:52:31 +0300

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Petros Gedekakis on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
